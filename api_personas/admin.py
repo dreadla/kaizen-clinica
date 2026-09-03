f78b31b579fac73f8a1ad168547d28a80ca3306f
Djonel <tu_correo@gmail.com>
2026-09-02 22:22:44 -0400

    Actualizar panel clinico

--- api_personas/admin.py
+++ api_personas/admin.py
@@ -2,7 +2,42 @@ from django.contrib import admin
 from .models import Paciente, Medico, Cita, Tratamiento
 
 
-admin.site.register(Paciente)
-admin.site.register(Medico)
-admin.site.register(Cita)
-admin.site.register(Tratamiento)
+@admin.register(Paciente)
+class PacienteAdmin(admin.ModelAdmin):
+    list_display = (
+        'rut',
+        'nombre',
+        'apellido',
+        'telefono',
+        'correo'
+    )
+
+
+@admin.register(Medico)
+class MedicoAdmin(admin.ModelAdmin):
+    list_display = (
+        'nombre',
+        'apellido',
+        'especialidad',
+        'telefono'
+    )
+
+
+@admin.register(Cita)
+class CitaAdmin(admin.ModelAdmin):
+    list_display = (
+        'paciente',
+        'medico',
+        'fecha',
+        'hora',
+        'estado'
+    )
+
+
+@admin.register(Tratamiento)
+class TratamientoAdmin(admin.ModelAdmin):
+    list_display = (
+        'paciente',
+        'fecha_inicio',
+        'fecha_fin'
+    )
