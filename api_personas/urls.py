6874c61cf0d52531bb4a423aa2ce80c3d8c78179
Djonel <tu_correo@gmail.com>
2026-09-02 22:44:03 -0400

    Actualizar panel clinico

--- api_personas/urls.py
+++ api_personas/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.inicio, name='inicio'),
+
+    path('', views.inicio, name="inicio"),
+
+    path('pacientes/', views.pacientes, name="pacientes"),
+
+    path('medicos/', views.medicos, name="medicos"),
+
+    path('citas/', views.citas, name="citas"),
+
 ]
--- api_personas/views.py
+++ api_personas/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from .models import Paciente, Medico, Cita
 
 
 def inicio(request):
     return render(request, "api_personas/inicio.html")
+
+
+def pacientes(request):
+    datos = Paciente.objects.all()
+    return render(request, "api_personas/pacientes.html", {"pacientes": datos})
+
+
+def medicos(request):
+    datos = Medico.objects.all()
+    return render(request, "api_personas/medicos.html", {"medicos": datos})
+
+
+def citas(request):
+    datos = Cita.objects.all()
+    return render(request, "api_personas/citas.html", {"citas": datos})
